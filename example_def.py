a2165a4de4b9daf57ec66155ab9dbae070982c3e
Lamcloud <59988710+Lamcloud@users.noreply.github.com>
2020-02-17 22:45:48 -0600

    Add files via upload

---
+++ example_def.py
@@ -0,0 +1,56 @@
+import typing
+from pprint import pprint
+from copy import deepcopy, copy
+
+
+def a (x: int, y: float) -> int:
+    """
+    A sample
+
+    :param x: x value
+    :type x: int
+    :param y: y value
+    :type y: float
+    :return: result
+    :return type: int
+    """
+    return 1
+
+# help(a)
+
+func = a
+
+# print ()
+
+for parameter, value in func.__annotations__.items():
+    if value==int:
+        print(parameter)
+
+
+# counter = 1
+# for f in [
+#     func.__name__,
+#     func.__code__.co_argcount,
+#     func.__code__.co_varnames,
+#     func.__code__.co_argcount,
+#     func.__code__.co_cellvars,
+#     func.__code__.co_code,
+#     func.__code__.co_consts,
+#     func.__code__.co_filename,
+#     func.__code__.co_firstlineno,
+#     func.__code__.co_flags,
+#     func.__code__.co_freevars,
+#     func.__code__.co_kwonlyargcount,
+#     func.__code__.co_lnotab,
+#     func.__code__.co_name,
+#     func.__code__.co_names,
+#     func.__code__.co_nlocals,
+#     func.__code__.co_posonlyargcount,
+#     func.__code__.co_stacksize,
+#     func.__code__.co_varnames,
+#     func.__doc__]:
+#     print (counter, f)
+#     counter +=1
+
+print (func.__annotations__)
+
